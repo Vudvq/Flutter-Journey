8d530889e257eb8d568a6c392d7e2f7cec23a042
unknown <sheraqib492@gmail.com>
2024-05-30 23:44:30 +0500

    Added new Widget

---
+++ 01-basic-widgets/Style and themes/Ui_helper/Text_theme.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+TextStyle mTextStyle11(
+{
+  Color textcolor=Colors.redAccent,
+  FontWeight fontWeight=FontWeight.w500
+
+}
+    ){
+  return TextStyle(
+    fontSize: 21,
+    color: textcolor,
+    fontWeight: fontWeight
+
+  );
+}
